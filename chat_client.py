7e3a1b260493a20c6df81980f11b342c5cc83d64
jorgevaz47 <jav200021@gmail.com>
2024-09-29 21:23:43 -0700

    Updating files

--- chat_client.py
+++ chat_client.py
@@ -3,10 +3,10 @@
 """Chat client for CST311 Programming Assignment 3"""
 __author__ = "[Stack Otterflow]"
 __credits__ = [
-  "Krishna Tagdiwala",
-  "Jorge Vazquez",
-  "Walid Elgammal",
-  "Jesus Martinez Miranda"
+    "Krishna Tagdiwala",
+    "Jorge Vazquez",
+    "Walid Elgammal",
+    "Jesus Martinez Miranda"
 ]
 
 # Import statements
@@ -14,54 +14,62 @@ import socket as s
 
 # Configure logging
 import logging
+
 logging.basicConfig()
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
 # Set global variables
-server_name = '10.0.0.1'
+server_name = '10.0.0.2'
 server_port = 12000
 
+
 def main():
-  # Create socket
-  client_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
-  
-  try:
-    # Establish TCP connection
-    client_socket.connect((server_name,server_port))
-  except Exception as e:
-    log.exception(e)
-    log.error("***Advice:***")
-    if isinstance(e, s.gaierror):
-      log.error("\tCheck that server_name and server_port are set correctly.")
-    elif isinstance(e, ConnectionRefusedError):
-      log.error("\tCheck that server is running and the address is correct")
-    else:
-      log.error("\tNo specific advice, please contact teaching staff and include text of error and code.")
-    exit(8)
-    
-  # Get input from user
-  user_input = input('Input lowercase sentence:')
-  
-  # Wrap in a try-finally to ensure the socket is properly closed regardless of errors
-  try:
-    # Set data across socket to server
-    #  Note: encode() converts the string to UTF-8 for transmission
-    client_socket.send(user_input.encode())
-    
-    # Read response from server
-    server_response = client_socket.recv(1024)
-    # Decode server response from UTF-8 bytestream
-    server_response_decoded = server_response.decode()
-    
-    # Print output from server
-    print('From Server:')
-    print(server_response_decoded)
-    
-  finally:
-    # Close socket prior to exit
+    # Create socket
+    client_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+
+    try:
+        # Establish TCP connection
+        client_socket.connect((server_name, server_port))
+    except Exception as e:
+        log.exception(e)
+        log.error("***Advice:***")
+        if isinstance(e, s.gaierror):
+            log.error("\tCheck that server_name and server_port are set correctly.")
+        elif isinstance(e, ConnectionRefusedError):
+            log.error("\tCheck that server is running and the address is correct")
+        else:
+            log.error("\tNo specific advice, please contact teaching staff and include text of error and code.")
+        exit(8)
+
+    # Get input from user
+    user_input = input('Input lowercase sentence:')
+
+    while user_input != "bye":
+        # Wrap in a try-finally to ensure the socket is properly closed regardless of errors
+        try:
+            # Set data across socket to server
+            #  Note: encode() converts the string to UTF-8 for transmission
+            client_socket.send(user_input.encode())
+
+            # Read response from server
+            server_response = client_socket.recv(1024)
+            # Decode server response from UTF-8 bytestream
+            server_response_decoded = server_response.decode()
+
+            # Print output from server
+            print('From Server:')
+            print(server_response_decoded)
+            # Get input from user
+            user_input = input('Input lowercase sentence:')
+        except:
+            print("Exception occurred when sending message")
+
     client_socket.close()
+
+
+
 
 # This helps shield code from running when we import the module
 if __name__ == "__main__":
-  main()
+    main()
--- chat_server.py
+++ chat_server.py
@@ -3,71 +3,82 @@
 """Chat server for CST311 Programming Assignment 3"""
 __author__ = "[Stack Otterflow]"
 __credits__ = [
-  "Krishna Tagdiwala",
-  "Jorge Vazquez",
-  "Walid Elgammal",
-  "Jesus Martinez Miranda"
+    "Krishna Tagdiwala",
+    "Jorge Vazquez",
+    "Walid Elgammal",
+    "Jesus Martinez Miranda"
 ]
-
 
 import socket as s
 import time
+import threading
 
 # Configure logging
 import logging
+
 logging.basicConfig()
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
 server_port = 12000
 
+users = []
+names = []
+
+
 def connection_handler(connection_socket, address):
-  # Read data from the new connectio socket
-  #  Note: if no data has been sent this blocks until there is data
-  query = connection_socket.recv(1024)
-  
-  # Decode data from UTF-8 bytestream
-  query_decoded = query.decode()
-  
-  # Log query information
-  log.info("Received query test \"" + str(query_decoded) + "\"")
-  
-  # Perform some server operations on data to generate response
-  time.sleep(10)
-  response = query_decoded.upper()
-  
-  # Sent response over the network, encoding to UTF-8
-  connection_socket.send(response.encode())
-  
-  # Close client socket
-  connection_socket.close()
-  
+    message = ""
+
+    while message != "bye":
+        try:
+            message = connection_socket.recv(1024)
+            send_message(connection_socket, message)
+        except:
+            print("Exception error when receiving data.")
+
+    users.remove(connection_socket)
+    connection_socket.close()
+
+
+def send_message(connection_socket, message):
+    for socket in users:
+        if socket is not connection_socket:
+            socket.send(message)
+
 
 def main():
-  # Create a TCP socket
-  # Notice the use of SOCK_STREAM for TCP packets
-  server_socket = s.socket(s.AF_INET,s.SOCK_STREAM)
-  
-  # Assign port number to socket, and bind to chosen port
-  server_socket.bind(('',server_port))
-  
-  # Configure how many requests can be queued on the server at once
-  server_socket.listen(1)
-  
-  # Alert user we are now online
-  log.info("The server is ready to receive on port " + str(server_port))
-  
-  # Surround with a try-finally to ensure we clean up the socket after we're done
-  try:
-    # Enter forever loop to listen for requests
-    while True:
-      # When a client connects, create a new socket and record their address
-      connection_socket, address = server_socket.accept()
-      log.info("Connected to client at " + str(address))
-      # Pass the new socket and address off to a connection handler function
-      connection_handler(connection_socket, address)
-  finally:
-    server_socket.close()
+    # Create a TCP socket
+    # Notice the use of SOCK_STREAM for TCP packets
+    server_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+
+    # Assign port number to socket, and bind to chosen port
+    server_socket.bind(('10.0.0.2', server_port))
+
+    # Configure how many requests can be queued on the server at once
+    server_socket.listen(1)
+
+    # Alert user we are now online
+    log.info("The server is ready to receive on port " + str(server_port))
+
+    # Surround with a try-finally to ensure we clean up the socket after we're done
+    try:
+        # Enter forever loop to listen for requests
+        while True:
+            # When a client connects, create a new socket and record their address
+            connection_socket, address = server_socket.accept()
+            log.info("Connected to client at " + str(address))
+
+            users.append(connection_socket)
+
+            # Setting up multiple threads to accept connections
+            thread = threading.Thread(target=connection_handler, args=(connection_socket, address))
+            thread.start()
+
+            # # Pass the new socket and address off to a connection handler function
+            # connection_handler(connection_socket, address)
+    finally:
+        server_socket.close()
+
 
 if __name__ == "__main__":
-  main()
+    main()
